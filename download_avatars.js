825335754d00ba83ec9dc79da714728feec6ab36
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-11 23:49:58 +0000

    defines function getRepoContributors and callback (and tests functions by hardcoding)

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,35 @@
 var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // fetch the list of contributors via HTTPS
+  var options = {
+    url: 'https://api.github.com/repos/jquery/jquery/contributors',
+    method: 'get',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      // cb is the callback passed in; and it takes in 2 arguments
+      // cb => function(err, result)
+      // good place to execute a callback is where we have access to the data it needs
+      cb(error, data);
+    }
+  }
+
+  // HTTPS request to GitHub
+  request(options, callback);
+};
+
+
+// takes in 3 arguments; first 2 arguments are to build the URL
+// third argument is for getting contributors from github API
+getRepoContributors("jquery", "jquery", function(err, contributors) {
+  console.log("Errors:", err);
+  console.log("Contributors:", contributors);
+});
